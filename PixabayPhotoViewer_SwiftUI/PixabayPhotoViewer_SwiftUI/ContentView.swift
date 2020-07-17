e5890910f8410cf2dbf85c092dbd33fe0ea2291d
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-07-17 22:55:35 +0900

    初回コミット

---
+++ PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PixabayPhotoViewer_SwiftUI
+//
+//  Created by home on 2020/07/17.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
