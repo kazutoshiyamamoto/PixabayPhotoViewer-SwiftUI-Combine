9d2539852361d42f6f1e70f67909c0d66bc16fa6
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-07-27 21:48:22 +0900

    画像取得時に使用するCodableに準拠した構造体を追加

---
+++ PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/Image/PixabayAPI/Responses.swift
@@ -0,0 +1,19 @@
+//
+//  Responses.swift
+//  PixabayPhotoViewer_SwiftUI
+//
+//  Created by home on 2020/07/27.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import Foundation
+
+struct ImageResponse: Codable {
+    var hits: [Hits]
+    struct Hits: Codable {
+        var previewURL: String
+        var tags: String
+        var user: String
+        var webformatURL: String
+    }
+}
