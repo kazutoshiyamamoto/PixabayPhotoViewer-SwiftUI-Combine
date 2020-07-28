c3c8bafe647c24adf76909b591aeefc05ce24047
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-07-28 23:49:29 +0900

    画像取得処理を実装するクラスを追加

---
+++ PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/Image/PixabayAPI/ImageFetcher.swift
@@ -0,0 +1,13 @@
+//
+//  ImageFetcher.swift
+//  PixabayPhotoViewer_SwiftUI
+//
+//  Created by home on 2020/07/28.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import Foundation
+
+class ImageFetcher {
+  
+}
