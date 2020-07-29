2e9252bf23d35aa9c1c384af57922322965416c0
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-07-29 23:35:26 +0900

    画像列の表示のために必要なロジックを実装するファイルを追加

---
+++ PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/Image/ImageList/ImageListRowViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ImageListRowViewModel.swift
+//  PixabayPhotoViewer_SwiftUI
+//
+//  Created by home on 2020/07/29.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import Foundation
