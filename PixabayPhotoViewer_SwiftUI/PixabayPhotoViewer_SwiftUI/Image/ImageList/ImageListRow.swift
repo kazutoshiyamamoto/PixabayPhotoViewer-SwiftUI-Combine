c8828c4e97559fd988f33be23bc580b07dcee098
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-07-31 22:42:49 +0900

    不要な実装を削除

--- PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/Image/ImageList/ImageListRow.swift
+++ PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/Image/ImageList/ImageListRow.swift
@@ -9,13 +9,6 @@
 import SwiftUI
 
 struct ImageListRow: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-}
-
-struct ImageListRow_Previews: PreviewProvider {
-    static var previews: some View {
-        ImageListRow()
     }
 }
