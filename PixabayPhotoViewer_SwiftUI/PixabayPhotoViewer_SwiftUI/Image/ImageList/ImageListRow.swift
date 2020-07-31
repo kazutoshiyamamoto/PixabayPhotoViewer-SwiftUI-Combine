d040561d903f8e551afc84a175fa5859498974e2
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-07-31 22:22:33 +0900

    画像列のViewを実装するファイルを追加

---
+++ PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/Image/ImageList/ImageListRow.swift
@@ -0,0 +1,21 @@
+//
+//  ImageListRow.swift
+//  PixabayPhotoViewer_SwiftUI
+//
+//  Created by home on 2020/07/31.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImageListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageListRow()
+    }
+}
