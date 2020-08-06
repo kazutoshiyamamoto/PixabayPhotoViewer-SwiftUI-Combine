60305490700bc53ad42486ac95d1e5993a802a51
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-08-06 17:43:42 +0900

    画像一覧画面用のViewModelを追加

---
+++ PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/Image/ImageList/ImageListViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ImageListViewModel.swift
+//  PixabayPhotoViewer_SwiftUI
+//
+//  Created by home on 2020/08/06.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImageListViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageListViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageListViewModel()
+    }
+}
