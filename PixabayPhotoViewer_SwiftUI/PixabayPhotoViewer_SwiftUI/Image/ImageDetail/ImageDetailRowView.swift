683963a0f844dadff17522f106999a18a04e0694
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-08-12 22:33:31 +0900

    画像詳細画面でViewModelを初期化

---
+++ PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/Image/ImageDetail/ImageDetailRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageDetailRowView.swift
+//  PixabayPhotoViewer_SwiftUI
+//
+//  Created by home on 2020/08/12.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImageDetailRowView: View {
+    private let viewModel: ImageDetailRowViewModel
+    
+    init(viewModel: ImageDetailRowViewModel) {
+        self.viewModel = viewModel
+    }
+    }
+}
