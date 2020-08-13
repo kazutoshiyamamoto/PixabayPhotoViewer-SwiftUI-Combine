5f3091d4d5af36e5a8d43719f86658144a11f78a
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-08-13 22:30:37 +0900

    不要な処理を削除

--- PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/Image/ImageDetail/ImageDetailView.swift
+++ PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/Image/ImageDetail/ImageDetailView.swift
@@ -16,12 +16,8 @@ struct ImageDetailView: View {
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ImageDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        ImageDetailView()
     }
 }
