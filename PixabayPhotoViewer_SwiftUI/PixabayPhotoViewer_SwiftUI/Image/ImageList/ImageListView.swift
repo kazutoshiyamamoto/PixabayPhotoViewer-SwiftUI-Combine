fd4e76e7984b9700e12d4a08f9aef014040d9bd7
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-08-17 12:17:27 +0900

    検索欄にアイコンを表示

--- PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/Image/ImageList/ImageListView.swift
+++ PixabayPhotoViewer_SwiftUI/PixabayPhotoViewer_SwiftUI/Image/ImageList/ImageListView.swift
@@ -36,6 +36,7 @@ private extension ImageListView {
     var searchField: some View {
         HStack(alignment: .center) {
             TextField("画像を検索", text: $viewModel.searchWord)
+            Image(systemName: "magnifyingglass")
         }
     }
     
